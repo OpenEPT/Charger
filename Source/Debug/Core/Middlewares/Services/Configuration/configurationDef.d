d2c8df923524fac9a63a3078b53f5ad88949c0dd
hturkmanovic <hturkmanovic@asensus-consultant.com>
2026-08-27 16:22:47 +0200

    Added .gitignore and documentation

--- Source/Debug/Core/Middlewares/Services/Configuration/configurationDef.d
+++
@@ -1,8 +0,0 @@
-Core/Middlewares/Services/Configuration/configurationDef.o: \
- ../Core/Middlewares/Services/Configuration/configurationDef.c \
- ../Core/Middlewares/Services/Configuration/configuration.h \
- ../Core/Configuration/globalConfig.h \
- ../Core/Middlewares/Services/Configuration/configurationDef.h
-../Core/Middlewares/Services/Configuration/configuration.h:
-../Core/Configuration/globalConfig.h:
-../Core/Middlewares/Services/Configuration/configurationDef.h:
--- Source/Debug/Core/Middlewares/Services/Control/CMParse/cmparse.d
+++
@@ -1,6 +0,0 @@
-Core/Middlewares/Services/Control/CMParse/cmparse.o: \
- ../Core/Middlewares/Services/Control/CMParse/cmparse.c \
- ../Core/Middlewares/Services/Control/CMParse/cmparse_util.h \
- ../Core/Middlewares/Services/Control/CMParse/cmparse.h
-../Core/Middlewares/Services/Control/CMParse/cmparse_util.h:
-../Core/Middlewares/Services/Control/CMParse/cmparse.h:
